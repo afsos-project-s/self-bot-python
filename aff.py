6bfbc62d6f8314c861096808addab393649ffbb0
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2022-11-16 13:48:23 +0300

    Create aff.py

---
+++ aff.py
@@ -0,0 +1,21 @@
+import discord, os, keep_alive, asyncio, datetime, pytz
+
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+
+
+
+
+@client.event
+async def on_connect():
+
+  await client.change_presence(activity = discord.Streaming(name = "I'm lookin' right at the other half of me 👀✨😍" , url = "https://top.gg/bot/813709481430089739"))
+
+
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
